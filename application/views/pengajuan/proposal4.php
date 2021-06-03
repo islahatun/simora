e6d1a8eac088b857a532ca0cba9e1c6dd506eca8
islahatun <islahatunnufusi98@gmail.com>
2021-06-04 00:34:46 +0700

    Menambahkan tampilan jadwal kegiatan

---
+++ application/views/pengajuan/proposal4.php
@@ -0,0 +1,89 @@
+<!-- Begin Page Content -->
+
+<!-- Page Heading -->
+<div class="card">
+    <div class="card-body">
+        <h3 class="mb-4"><?= $judul; ?></h3>
+        <form action="<?= base_url('pengajuan/proposal4/') ?><?= $rak['id'] ?>" method="post">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Tanggal</label>
+                        <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="tanggal">
+                        <?= form_error('tanggal', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Waktu Mulai</label>
+                        <input type="time" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="mulai">
+                        <?= form_error('mulai', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Waktu Selesai</label>
+                        <input type="time" class=" form-control" id="exampleInputPassword1" name="selesai">
+                        <?= form_error('selesai', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Nama Kegiatan</label>
+                        <input type="text" class=" form-control" id="exampleInputPassword1" name="kegiatan">
+                        <?= form_error('kegiatan', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <input type="text" class=" form-control" id="exampleInputPassword1" name="id_pengguna" value="<?= $pengguna['id'] ?>" hidden>
+                        <?= form_error('id_pengguna', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <input type="text" class=" form-control" id="exampleInputPassword1" name="id_rak" value="<?= $rak['id'] ?>" hidden>
+                        <?= form_error('id_rak', '<small class="text-danger pl-3">', ' </small>') ?>
+                    </div>
+                </div>
+            </div>
+            <div class="text-right">
+                <button type="submit" class="btn btn-dark">Submit</button>
+            </div>
+
+        </form>
+        <hr>
+        <div class="col-4">
+
+        </div>
+        <form action="">
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr class="text-center">
+                        <th scope="col">Tanggal</th>
+                        <th scope="col">Waktu</th>
+                        <th scope="col">Nama Kegiatan</th>
+                        <th scope="col">Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr colspan=3>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="text-right">
+                <a href="<?= base_url('pengajuan/proposal3/') ?><?= $rak['id'] ?>" class="btn btn-dark mb-2 ">Kirim</a>
+            </div>
+        </form>
+
+
+    </div>
+</div>
+</div>
